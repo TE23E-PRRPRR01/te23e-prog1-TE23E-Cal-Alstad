6345eac374e6bedfef531a3d380dd19d02e40365
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2024-08-26 14:31:50 +0200

    Tränat på cw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Nu mitt första C#-program");
+
+Console.Write("Vad heter du? ");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej! "+ namn);
+
+Console.Write("Vad gillar du att göra på fritiden? ");
+string intresse = Console.ReadLine();
+Console.WriteLine("Jag gillar också " + intresse);
+
+Console.BackgroundColor = ConsoleColor.Magenta;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Vad tyckte du om OS i sommar? ");
+Console.WriteLine("Vilken sport tyckte du var bästa? ");
