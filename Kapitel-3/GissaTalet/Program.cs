339b88a2ac86cf8b55a2fc87a35af1836c6fcb9b
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2024-09-09 14:50:01 +0200

    tränat på heltal och konvertering med int.parse()

---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,27 @@
+﻿// Ett litet spel - Gissa ett hemligt heltal
+Console.Clear();
+Console.WriteLine("Ett litet spel - Gissa ett hemligt heltal");
+
+//Slumpa ett tal
+int slumptalet = Random.Shared.Next(1, 101); // 1-100
+Console.WriteLine($"Slumptalet är {slumptalet}");
+
+Console.Write("Gissa ett tal 1-100: ");
+// Läs in texten, tex "100" 
+ string gissningText = Console.ReadLine();
+
+ // Konvertera texten till ett heltal, blir 100
+int gissning = int.Parse(gissningText);
+
+if (gissning == slumptalet)
+{
+    Console.WriteLine("Bra, du har gissat rätt");
+}
+else if (gissning < slumptalet)
+{
+    Console.WriteLine("Fel, du gissade för lågt, försök igen!");
+}
+else if (gissning > slumptalet )
+{ 
+    Console.WriteLine("Fel, du gissade för högt, försök igen!");
+}
---
+++ Kapitel-3/ÅldersGräns/Program.cs
@@ -0,0 +1,37 @@
+﻿// Åldersgränser i USA
+using System.Runtime.CompilerServices;
+
+Console.Clear();
+Console.WriteLine("Vad får du göra i USA?" );
+
+//Fråga efter ålder
+Console.Write("Hur gammal är du?: ");
+
+//läs in texten
+string ageText = Console.ReadLine();
+//konvertera till heltal
+int age = int.Parse(ageText);
+
+if (age >= 21)
+{
+    Console.WriteLine("Du får köra bil, gå på kasino, samt dricka alkohol och använda tobak, du får även röka på i vissa delstater som exempelvis Michigan och Kalifornien![samt allt du fick när du var 18]");
+}
+else if (age >= 18) 
+{
+    Console.WriteLine("Du får ha sex i alla delstater, du får rösta i alla val både statliga och federala samt gå med i militären [allt du fick göra när du var 16 förutom att ha sex med minderåriga]");
+}
+else if (age >= 16)
+{
+    Console.WriteLine("Du får skaffa ett ID-kort, arbeta deltid i alla stater samt skaffa körkort och köra bil [du får göra allt du fick när du var 14]");
+}
+else if (age >= 14)
+{
+    Console.WriteLine("Du får skaffa ett lärlingskörkort, du får jobba deltid i vissa stater, samt bestämma över din egna medicinska beslut");
+}
+else if (age < 14)
+{
+    Console.WriteLine("Du har absolut ingen frihet och närstintill inga rättigheter ha så kul!");
+} 
+
+
+    
