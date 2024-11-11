7262707359c0f9f86567f478f4a7b2ae5cd6bd22
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2024-11-11 14:54:17 +0100

    övat med metoder och try.Parse

---
+++ Kapitel-5/SlumpadLista/Program.cs
@@ -0,0 +1,30 @@
+﻿// Slumpa många tal och spara i lista
+using System.ComponentModel.DataAnnotations;
+Console.Clear();
+Console.WriteLine("Slumpa många tal och spara i lista");
+
+List<int> listaSlumptal = [];
+
+// be användaren ange antal slumptal
+Console.Write("Ange antal slumptal: ");
+int antal = int.Parse(Console.ReadLine());
+
+// Be användaren ange min och max slumptal
+Console.Write("Ange minsta värde för slumptal: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Ange högsta värde för slumptal: ");
+int max = int.Parse(Console.ReadLine());
+// loppa 5 ggr 
+for (int i = 0; i< antal; i++)
+{        
+// slumpa ett tal 1-100
+int slumptal = 0;
+slumptal = Random.Shared.Next(min, max+1);
+
+// lägg till slumptalet i listan
+
+listaSlumptal.Add(slumptal);
+Console.WriteLine($"Slumpat tal {i+1}: {slumptal}");
+
+}
+
---
+++ Kapitel-5/SlumpadLista2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Skapa lista med slumpade tal
+
+/********************************************************************************
+**                                   Main                                      **
+*********************************************************************************/
+
+
+// Validera användarens inmatning
+Console.Clear();
+
+// Skapar en variabel för antalet slumptal för global användning
+int antal = LäsInHeltal();
+int min = LäsInHeltal();
+int max= LäsInHeltal();
+
+// Deklarera variabler
+// Skapa en lista för att spara de slumpade talen
+List<int> listaSlumptal = [];
+
+
+
+// Loopar {antal} gånger
+for (var i = 0; i < antal; i++)
+{
+    // Slumpar ett mellan {min} och {max}
+    int slumptal = Random.Shared.Next(min, max + 1);
+
+    // Lägger in slumptalet i en lista
+    listaSlumptal.Add(slumptal);
+
+    // Skriver ut
+    Console.WriteLine($"Slumpat tal nummer {i + 1} är: {slumptal}");
+}
+
+/********************************************************************************
+**                             Mina egna metoder                               **
+*********************************************************************************/
+
+static int LäsInHeltal()
+{
+    int heltal = 0;
+    while (true)
+    {
+        // Fråga användaren hur många slumptal som ska skapas
+        Console.Write("Ange heltal: ");
+
+        bool lyckades = int.TryParse(Console.ReadLine(), out heltal);
+        if (lyckades)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Du måste skriva ett heltal");
+        }
+
+    }
+    return heltal;
+}
+
