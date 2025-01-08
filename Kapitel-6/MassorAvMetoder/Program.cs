1eb0fe2fac27e199702449b0ee2db610f3374edd
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2025-01-08 15:03:23 +0100

    Massor av metoder

---
+++ Kapitel-6/MassorAvMetoder/Program.cs
@@ -0,0 +1,36 @@
+﻿/******************************************************************
+*                              M A I N                            *
+*******************************************************************/
+
+Console.Clear();
+Console.WriteLine("Hej Metoder!");
+
+SägHejNamn("Dante");
+KvadreraNummer(5);
+
+
+/******************************************************************
+*                           M E T O D E R                         *
+*******************************************************************/
+
+/// <summary>
+/// Skriv ut hälsning
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+   //Console.Write("Ange namn: ");
+   //string namn = Console.ReadLine();
+   Console.WriteLine($"Hej på dig {namn}!");
+
+}
+
+/// <summary>
+/// Metod som skriver ut tal i kvadrat
+/// </summary>
+/// <param name="nummer">Numret</param>
+static void KvadreraNummer(int nummer)
+{
+    Console.WriteLine($"{nummer} kvadrerat blir {nummer * nummer}");
+
+}
---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿/******************************************************************
+*                              M A I N                            *
+*******************************************************************/
+
+// Intro till metoder
+using System.Runtime.InteropServices;
+
+Console.Clear();
+Console.WriteLine("Hej Metoder!");
+
+//Anropa metoden 
+SägHej();
+
+// Anropa metoden Meny
+VisaMeny();
+
+//Anropa metoden RegisterarNamn()
+RegistreraNamn();
+
+
+
+/******************************************************************
+*                           M E T O D E R                         *
+*******************************************************************/
+
+/// <summary>
+/// Metod som skriver ut hälsning
+/// </summary>
+static void SägHej()
+{
+    Console.WriteLine("Hej på dig!");
+}
+
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut alla uppgifter
+    4. Avsluta program
+    """);
+}
+
+static void RegistreraNamn()
+{
+    Console.Write("Vad är ditt namn: ");
+    string namn = Console.ReadLine();
+    Console.WriteLine($"Hej {namn}");
+    
+}
