ffa544a5800bfe6a7374865e47d9fc0307b3e7b3
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2025-01-15 15:04:43 +0100

    tränat på metoder som returnerar

---
+++ Kapitel-6/MetoderMedParametrar/Program.cs
@@ -0,0 +1,131 @@
+﻿/****************************************************************************
+*                                 M A I N                                   *
+****************************************************************************/
+Console.Clear();
+ 
+Console.WriteLine("Program med metoder");
+ 
+//********************************
+// SKRIV UT MEDDELANDE VERTIKALT *
+//********************************
+ 
+//Om vi inte hade haft metoder hade vi skrivit såhär:
+ 
+string meddelande = "Måndag"; // Variabel för meddelande som ska loopas
+ 
+for (var i = 0; i < meddelande.Length; i++) // Loopar lika många gånger som bokstäver i "Måndag"
+{
+    Console.WriteLine(meddelande[i]); // Skriver ut det i konsolen.
+}
+ 
+// I och med att vi har metoder, så kan man även skriva såhär:
+ 
+SkrivVertikalt("Tisdag"); // Kallar på metoden 'SkrivVertikalt' med innehållet "Tisdag" för att definiera vad som ska skrivas ut
+ 
+//***************************************************
+// SKRIV UT FYRKANT MED SYMBOLER - utan kommentarer *
+//***************************************************
+ 
+Console.Clear(); // Kommentera ut den här om du vill se resultatet av resterande kod
+ 
+RitaFyrkant(3);
+ 
+//****************************************************************
+// SKRIV UT MEDDELANDE ETT VISST ANTAL GÅNGER - utan kommentarer *
+//****************************************************************
+ 
+Console.Clear(); // Kommentera ut den här om du vill se resultatet av resterande kod
+ 
+SkrivMeddelande("Detta kommer upprepas så här många gånger --> ", 5);
+ 
+//****************************************************************
+// SKRIV UT TEMP FRÅN CELSIUS TILL FAHRENHEIT - utan kommentarer *
+//****************************************************************
+ 
+Console.Clear(); // Kommentera ut den här om du vill se resultatet av resterande kod
+ 
+OmvandlaCelsius(5);
+ 
+//************************************************
+// SLÅ TÄRNING (SLUMPTAL) ETT VISST ANTAL GÅNGER *
+//************************************************
+ 
+SlåTärning(Random.Shared.Next(1, 7), 5); // Metod som kallar en sexsiffrig tärning (random) som ska kastas 5 gånger
+ 
+/****************************************************************************
+*                              M E T O D E R                                *
+****************************************************************************/
+ 
+/// <summary>
+/// Metod för att skriva ut vertikalt
+/// </summary>
+/// <param name="meddelande1">Vertikalt meddelande</param>
+static void SkrivVertikalt(string meddelande1) // En metod med namnet 'SkrivVertikalt' som skapar en ny string inuti sig, 'meddelande1'
+{
+    // En till likadan for-loop som tidigare, fast med metodens string istället för 'meddelande' (gamla)
+    for (var i = 0; i < meddelande1.Length; i++)
+    {
+        Console.WriteLine(meddelande1[i]); // Skriver ut 'meddelande1' (nya) på samma sätt som 'meddelande' (gamla)
+    }
+}
+ 
+/// <summary>
+/// Metod för att skriva ut en kvadrat
+/// </summary>
+/// <param name="sidlängd">Längd på sida</param>
+static void RitaFyrkant(int sidlängd)
+{
+    for (var i = 0; i < sidlängd; i++)
+    {
+        for (var j = 0; j < sidlängd; j++)
+        {
+            Console.Write("■ ");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+/// <summary>
+/// Metod för att skriva ut meddelande ett visst antal gånger
+/// </summary>
+/// <param name="text">Text i meddelande</param>
+/// <param name="antal">Antal upprepningar</param>
+static void SkrivMeddelande(string text, int antal)
+{
+ 
+    for (var i = 0; i < antal; i++)
+    {
+        Console.WriteLine(text + antal);
+    }
+}
+ 
+/// <summary>
+/// Omvandla Celsius till Fahrenheit
+/// </summary>
+/// <param name="temp">Temperatur</param>
+static void OmvandlaCelsius(int temp)
+{
+    Console.Write($"{temp}°C motsvarar {temp * 9 / 5 + 32}F");
+}
+ 
+/// <summary>
+/// Omvandla Celsius till Fahrenheit (ineffektivt sätt att göra det på)
+/// </summary>
+/// <param name="celsius">Temperatur i Celsius</param>
+/* void OmvandlaCelsiusIneffektiv(int celsius) {
+    int fahrenheit = celsius * 9/5 + 32;
+    Console.Write($"{celsius}°C motsvarar {fahrenheit}F");
+}*/
+ 
+/// <summary>
+/// Slå en tärning ett visst antal gånger
+/// </summary>
+/// <param name="randomNummer">Slumptal - definierar tärningens storlek</param>
+/// <param name="antalKast">Antal kast med tärning</param>
+static void SlåTärning(int randomNummer, int antalKast)
+{
+    for (var i = 0; i < antalKast; i++) // For-loop som loopar 'antalKast' gånger
+    {
+        Console.WriteLine(randomNummer); // Skriver ut random-nummer baserat på inmatning i metodens kallelse
+    }
+}
---
+++ Kapitel-6/MetoderSomReturnerar/Program.cs
@@ -0,0 +1,128 @@
+﻿/******************************************************************
+*                              M A I N                            *
+*******************************************************************/
+//Använd Metoderna Här!
+
+/*int svar = Addera(3, 5);
+Console.WriteLine($"Svaret är {svar}");
+Console.WriteLine(Addera(3, 5));*/
+Console.WriteLine($"3 + 5 = {Addera(3, 5)}");
+
+Console.Write("Skriv den text du vill vända: ");
+string texten = Console.ReadLine();
+Console.WriteLine($"{texten} blir vänt {VändText(texten)}");
+
+Console.WriteLine($"3 * 5 = {Multiplicera(3, 5)}");
+
+Console.WriteLine($"3 / 5 = {Dividera(3, 5)}");
+
+Console.WriteLine($"Längst av Hej och Hejsan är {Längst("Hej", "Hejsan")}");
+
+Console.Write("Ange ett tal: ");
+int nyTal1 = LäsInHeltal();
+
+Console.Write("Ange ett tal: ");
+int nyTal2 = LäsInHeltal();
+
+/******************************************************************
+*                           M E T O D E R                         *
+*******************************************************************/
+//Skapa Metoder Här!
+
+/// <summary>
+/// Summera två tal
+/// </summary>
+/// <param name="tal1"></param>
+/// <param name="tal2"></param>
+/// <returns>Summan</returns>
+static int Addera(int tal1, int tal2) //signatur
+{
+    int summa = tal1 + tal2;
+    return summa;
+}
+
+/// <summary>
+/// Vänder på texten
+/// </summary>
+/// <param name="text"></param>
+static string VändText (string text)
+{
+    string omvändText = "";
+    for (int i = text.Length - 1; i >= 0 ; i--)
+    {
+        omvändText += text[i];
+    }
+    return omvändText;
+}
+
+/// <summary>
+/// Multiplicera två tal
+/// </summary>
+/// <param name="tal1"></param>
+/// <param name="tal2"></param>
+/// <returns></returns>
+static double Multiplicera (double tal1, double tal2)
+{
+    double summa2 = tal1 * tal2;
+    return summa2;
+}
+
+/// <summary>
+/// Dividera två tal
+/// </summary>
+/// <param name="tal1"></param>
+/// <param name="tal2"></param>
+/// <returns></returns>
+static double Dividera (double tal1, double tal2)
+{
+    double summa3 = tal1 / tal2;
+    return summa3;
+}
+
+/// <summary>
+/// Returnera det längsta string
+/// </summary>
+/// <param name="text1"></param>
+/// <param name="text2"></param>
+/// <returns></returns>
+static string Längst(string text1, string text2)
+{
+    if (text2.Length > text1.Length)
+    {
+        return text2;
+    }
+    else
+    {
+        return text1;
+    }
+}
+
+/// <summary>
+/// Säkert sätt att läsa in ett tal (Utan att det krashar)
+/// </summary>
+/// <param name="args"></param>
+static int LäsInHeltal()
+{
+    int tal = 0;
+    while (true)
+    {
+        //Läs in från användaren
+        string textSomBlirTal = Console.ReadLine();
+
+        //Kolla om texten är ett tal
+         bool lycakdes = int.TryParse(textSomBlirTal, out tal);
+
+         if (lycakdes == true)
+         {
+            break;
+         }
+         else
+         {
+            Console.WriteLine("Felaktig inmatning, var vänlig ange ett heltal!");
+         }
+    }
+   
+
+    return tal;
+}
+
